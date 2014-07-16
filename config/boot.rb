c9ef54d5896b0b9f17694a373715da585e6f8be1
Eric Wollesen <ericw@xmtp.net>
2014-07-15 19:47:00 -0600

    Let's use port 16000

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,16 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+
+# Use a default port other than 3000
+require "rails/commands/server"
+
+module Rails
+  class Server
+    alias :default_options_alias :default_options
+    def default_options
+      default_options_alias.merge!(:Port => 16000)
+    end
+  end
+end
